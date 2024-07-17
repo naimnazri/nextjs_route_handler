ab00edf3c1726ce52d91420be9eaec7018eea734
Naim <nazrinaim@gmail.com>
2024-07-17 19:06:01 +0800

    update progress get

---
+++ src/app/comments/data.ts
@@ -0,0 +1,16 @@
+export const comments = [
+
+    {
+        id: 1,
+        text: "This is the first comment",
+    },
+    {
+        id: 2,
+        text: "This is the seoond comment",
+    },
+    {
+        id: 3,
+        text: "This is the third comment",
+    },
+    
+];
